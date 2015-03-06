c592fa99db7862ec603accb1b8eb4cc49e19e2e8
Andrii Gerasymchuk <gera3dartist@gmail.com>
2015-03-06 02:28:17 +0200

    added microblog app

---
+++ app/__init__.py
@@ -0,0 +1,73 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+mail = Mail(app)
+
+db = SQLAlchemy(app)
+
+from flask.ext.babel import Babel
+babel = Babel(app)
+
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+from app import views, models
+
+# if not app.debug:
+# 	import logging
+# 	from logging.handlers import SMTPHandler
+# 	credentials = None
+# 	if MAIL_USERNAME or MAIL_PASSWORD:
+# 		credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+# 	mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@'+\
+# 		MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+# 	mail_handler.setLevel(logging.ERROR)
+# 	app.logger.addHandler(mail_handler)
+
+if not app.debug:
+	import logging
+	from logging.handlers import RotatingFileHandler
+	file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1*1024*1024,10)
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'\
+		+ '[in %(pathname)s:%(lineno)d]'))
+	app.logger.setLevel(logging.INFO)
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+	app.logger.info('microblog startup')
+
+from .momentjs import momentjs
+app.jinja_env.globals['momentjs'] = momentjs
+
+from flask.ext.babel import lazy_gettext
+lm.login_message = lazy_gettext('Please log in to access this page.')
+
+
+from flask.json import JSONEncoder
+
+class CustomJSONEncoder(JSONEncoder):
+	"""
+	This class adds support for lazy translation texts to Flask's
+	JSON encoder. This is necessary when flashing translated texts.
+	"""
+	def default(self, obj):
+		from speaklater import is_lazy_string
+		if is_lazy_string(obj):
+			try:
+				return unicode(obj) # python 2
+			except NameError:
+				return str(obj) # python 3
+		return super(CustomJSONEncoder, self).default(obj)
+
+app.json_encoder = CustomJSONEncoder
