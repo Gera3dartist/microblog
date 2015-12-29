06026719343023e62757e0ca6c68fd9c6f464285
Andrii Gerasymchuk <gera3dartist@gmail.com>
2015-12-29 02:10:23 +0200

    configured db: using mysql

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config['MYSQL_DATABASE_USER'] = 'apps'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Dhn3EicNdi'
+app.config['MYSQL_DATABASE_DB'] = 'apps'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql = MySQL()
+mysql.init_app(app)
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
--- config.py
+++ config.py
@@ -1,4 +1,28 @@
+import os
+
 __author__ = 'agerasym'
+
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'some+V3RYSeCR3TKey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'mysql://apps:Dhn3EicNdi@localhost/apps'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'https://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'https://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'},
+]
+
+# database config
+# MYSQL_DATABASE_USER = 'apps'
+# MYSQL_DATABASE_PASSWORD = 'Dhn3EicNdi'
+# MYSQL_DATABASE_DB = 'apps'
+# MYSQL_DATABASE_HOST = 'localhost'
+
+# SQL_ALCHEMY_DATABASE_URI =
+
+
