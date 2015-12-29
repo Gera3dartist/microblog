de2c4001c7c7a2fd42d5554b7dbfda7a07001ddd
Andrii Gerasymchuk <gera3dartist@gmail.com>
2015-12-29 02:14:54 +0200

    tweaked login view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
 from app.forms import LoginForm
 from flask import render_template
+from flask import flash
+from flask import redirect
 
 
 @app.route('/')
@@ -28,6 +30,11 @@ def about():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested: {}, remember me: {}'.format(
+            form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
